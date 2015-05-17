6d5a5c4406dc9d3fd588c7a7f549145a1d4f6c7f
tempaowca <artus.sawicki@gmail.com>
2015-05-17 20:20:16 +0200

    adding controler for adding movies

---
+++ FluffyBunnyFeet/AddViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddViewController.h
+//  FluffyBunnyFeet
+//
+//  Created by Agnieszka Chudek i Adam Dziedzic on 17.05.2015.
+//  Copyright (c) 2015 Operation Sleepy Weasel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddViewController : UIViewController
+- (IBAction)dismiss:(id)sender;
+
+@end
