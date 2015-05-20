72584ed58a3df349aac14a0aff88f71a3e5cf112
tempaowca <artus.sawicki@gmail.com>
2015-05-20 16:24:34 +0200

    removing comments

--- FluffyBunnyFeet/SearchTableViewController.m
+++ FluffyBunnyFeet/SearchTableViewController.m
@@ -12,14 +12,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     self.foundMovies = [[NSMutableArray alloc] init];
-    [self fetchGreeting];
+    [self fetchMovies];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,7 +21,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)fetchGreeting;
+- (IBAction)fetchMovies;
 {
     NSString *url = @"";
     NSString *apiKey = @"8ec7476e88d48365fd343370b9947b76";
@@ -94,41 +88,6 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -138,6 +97,5 @@
         self.selectedResult = self.foundMovies[indexPath.row];
     }
 }
-
 
 @end
